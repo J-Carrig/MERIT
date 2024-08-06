1d9bc0eef0642f76a756a23f9dd146f87eb5a4fc
J-Carrig <jcarrig@tcd.ie>
2024-08-06 15:12:56 +0100

    changed varargin to argument blocks

--- +merit/+beamform/get_delays.m
+++ +merit/+beamform/get_delays.m
@@ -1,19 +1,29 @@
-function [calculate_time] = get_delays(channels, antennas, varargin),
-  c_0 = 299792458;
-  p = inputParser;
-  addOptional(p, 'relative_permittivity', 1, @(a) isnumeric(a) && numel(a) == 1 && a >= 1);
-  parse(p, varargin{:});
-  relative_permittivity = p.Results.relative_permittivity;
-
-  speed = c_0./sqrt(relative_permittivity);
-
-  antennas = antennas';
+function [calculate_time] = get_delays(channels, antennas, options)
   
-  function [time] = calculate_(points),
-    points = permute(points, [2, 3, 1]);
-    distances = sqrt(sum(bsxfun(@minus, antennas, points).^2, 1));
+arguments
+  channels                                                                                                                          % list of channels
+  antennas                                                                                                                          % list of antenna locations
+  options.relative_permittivity {mustBeNumeric, mustBeScalarOrEmpty, mustBeGreaterThanOrEqual(options.relative_permittivity,1)}     % relative permittivity must be a nummeric scaler >= 1
+end
+  c_0 = 299792458;  % speed of light in a vacuum
+  relative_permittivity = options.relative_permittivity;
 
-    time = -bsxfun(@rdivide, distances(:, channels(:, 1), :) + distances(:, channels(:, 2), :), speed);
+  speed = c_0./sqrt(relative_permittivity); %speed relative to speed of light
+
+  antennas = antennas'; % transpose antennas array
+  
+  function [time] = calculate_(points)
+
+
+    % permute: lets say points is a 4 x 5 x 6 (4 rows, 5 columns, 6 pages) matrix
+    % permute (points [2,3,1]) tells us that there are 4 columns, 5 pages
+    % and 6 rows in the new points array
+    
+
+    points = permute(points, [2, 3, 1]);
+    distances = sqrt(sum( (antennas - points).^2, 1) );
+
+    time = - ( distances(:, channels(:, 1), :) + distances(:, channels(:, 2), :) ) / speed;
   end
   calculate_time = @calculate_;
 end
--- +merit/beamform.m
+++ +merit/beamform.m
@@ -1,14 +1,19 @@
-function [img, sigs] = beamform(signals, axis_, points, delay, image_, varargin),
-  p = inputParser;
-  addOptional(p, 'max_memory', 1e9, @isnumeric);
-  addOptional(p, 'gpu', false, @islogical);
-  addOptional(p, 'window', @(a) a);
-  parse(p, varargin{:});
-  gpu = p.Results.gpu;
-  max_memory = p.Results.max_memory;
-  window = p.Results.window;
+function [img, sigs] = beamform(signals, axis_, points, delay, image_, options)
+arguments
+    signals 
+    axis_ 
+    points 
+    delay 
+    image_ 
+    options.max_memory {mustBeNumeric} = 1e9
+    options.gpu {mustBeNumericOrLogical} = false
+    options.window = @(a) a
+end
+  gpu = options.gpu;
+  max_memory = options.max_memory;
+  window = options.window;
 
-  if isreal(signals),
+  if isreal(signals)
     bytes = 8;
   else
     bytes = 16;
@@ -23,7 +28,7 @@ function [img, sigs] = beamform(signals, axis_, points, delay, image_, varargin)
     delay = {delay};
   end
 
-  if gpu,
+  if gpu
     try
       dev = gpuDevice();
       free_mem = dev.FreeMemory;
@@ -49,8 +54,8 @@ function [img, sigs] = beamform(signals, axis_, points, delay, image_, varargin)
   for r = 1:points_run:nPoints,
     for d = 1:numel(delay)
       rng = r:min(nPoints, r+points_run-1);
-      img(rng, d, :) = image_(window(...
-        merit.process.delay(signals, delay{d}(points(rng, :)), axis_)));
+      delayed_signals = merit.process.delay(signals, delay{d}(points(rng, :)), axis_);
+      img(rng, d, :) = image_(window(delayed_signals));
     end
   end
 
